d6019aa7c266b2a224b55858e6919e0a4b0cdb20
Foster Stule <camodude5@yahoo.com>
2018-05-29 13:39:01 -0400

    inital commit with students

--- HelloWorldProject/Program.cs
+++ HelloWorldProject/Program.cs
@@ -10,6 +10,24 @@ namespace HelloWorldProject
 	{
 		static void Main(string[] args)
 		{
+			Console.WriteLine("Hello, World!");
+
+			Student foster = new Student();
+			foster.SetFirstname("Foster");
+			foster.SetLastname("Stulen");
+			foster.setID(123);
+			String fn;
+			fn=foster.GetFirstname();
+			string ln = foster.GetLastname();
+			foster.SetGpa(4.02);
+			foster.setSat(1200);
+			foster.setIsFemale(false);
+			int id = foster.getId();
+			int sat = foster.getSat();
+			double gpa = foster.getGpa();
+			bool female = foster.getIsfemale();
+			Console.WriteLine(fn+" "+ln +" "+id+" "+sat+" "+gpa+" "+female);
+			
 		}
 	}
 }
---
+++ HelloWorldProject/Student.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldProject
+{
+	class Student
+	{
+		string firstName;
+		string lastName;
+		int id;
+		double gpa;
+		int SAT;
+		bool IsFemale;
+
+		public String GetFirstname()
+		{
+			return firstName;
+		}
+		public string GetLastname()
+		{
+			return lastName;
+		}
+		public void SetFirstname(string fn)
+		{
+			firstName = fn;
+		}
+		public void SetLastname(string ln)
+		{
+			lastName = ln;
+		}
+		public void setID(int num)
+		{
+			id = num;
+		}
+		public int getId()
+		{
+			return id;
+		}
+		public void SetGpa(double pGpa)
+		{
+			gpa = pGpa;
+		}
+		public double getGpa()
+		{
+			return gpa;
+		}
+		public void setSat(int psat)
+		{
+			SAT = psat;
+		}
+		public int getSat()
+		{
+			return SAT;
+		}
+		public void setIsFemale(bool morf)
+		{
+			IsFemale = morf;
+		}
+		public bool getIsfemale()
+		{
+			return IsFemale;
+		}
+	}
+
+}
